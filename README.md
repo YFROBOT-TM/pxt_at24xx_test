57aabe38741f6db99f6168b9537917aaea593d1e
shaoziyang <shaoziyang@126.com>
2018-05-29 22:48:14 +0800

    初始版本

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# AT24CXX_cn
-AT24CXX eeprom 存储器的 PXT 软件包
+# AT24XX
+
+AT24CXX eeprom pxt 软件包
+
+作者: shaoziyang  
+时间: 2018年5月  
+
+![](icon.png)  
+
+
+## 使用方法
+
+打开 makecode 编辑器，在项目中选择添加软件包，然后在地址栏输入下面网址  
+
+https://github.com/microbit-makecode-packages/AT24CXX  
+
+搜索后就可以添加并使用本软件包了。
+
+## I2C 地址  
+
+- 80
+
+## API
+
+- **function write_byte(addr: number, dat: number)**  
+写入一个字节到指定地址
+
+- **function read_byte(addr: number)**  
+从指定地址读取一个字节
+
+- **function write_word(addr: number, dat: number)**  
+写入一个字到指定地址
+
+- **function read_word(addr: number)**  
+从指定地址读取一个字
+
+- **function write_dword(addr: number, dat: number)**  
+写入一个双字到指定地址
+
+- **function read_dword(addr: number)**  
+从指定地址读取一个双字
+
+
+## 演示
+
+![](demo.jpg)
+
+## 授权方式
+
+MIT
+
+microbit/micropython 中文社区版权所有 (c) 2018  
+
+## 支持硬件
+
+* for PXT/microbit
+
+
+[来自 microbit/micropython 中文社区](http://www.micropython.org.cn)  
---
+++ main.ts
@@ -0,0 +1,95 @@
+/**
+ * makecode AT24CXX EEPROM Package.
+ * From microbit/micropython 中文社区.
+ * http://www.micropython.org.cn
+ */
+
+/**
+ * AT24CXX block
+ */
+//% weight=100 color=#303030 icon="\uf2db" block="AT24CXX"
+namespace AT24CXX {
+    let AT24_I2C_ADDR = 80;
+
+    /**
+     * 写入一个字节到指定地址
+     * @param addr eeprom address, eg: 1
+     * @param dat is the data will be write, eg: 5
+     */
+    //% blockId="AT24_WriteByte" block="eeprom 地址 %addr|写字节 %dat"
+    //% weight=100 blockGap=8
+    export function write_byte(addr: number, dat: number): void {
+        let buf = pins.createBuffer(3);
+        buf[0] = addr >> 8;
+        buf[1] = addr;
+        buf[2] = dat;
+        pins.i2cWriteBuffer(AT24_I2C_ADDR, buf)
+    }
+
+    /**
+     * 从指定地址读取字节
+     * @param addr eeprom address, eg: 1
+     */
+    //% blockId="AT24_ReadByte" block="读取字节自地址 %addr"
+    //% weight=99 blockGap=8
+    export function read_byte(addr: number): number {
+        pins.i2cWriteNumber(AT24_I2C_ADDR, addr, NumberFormat.UInt16BE);
+        return pins.i2cReadNumber(AT24_I2C_ADDR, NumberFormat.UInt8BE);
+    }
+
+    /**
+     * 写入字到指定地址
+     * @param addr eeprom address, eg: 2
+     * @param dat is the data will be write, eg: 6
+     */
+    //% blockId="AT24_WriteWord" block="eeprom 地址 %addr|写入字 %dat"
+    //% weight=90 blockGap=8
+    export function write_word(addr: number, dat: number): void {
+        let buf = pins.createBuffer(4);
+        buf[0] = addr >> 8;
+        buf[1] = addr;
+        buf[2] = dat >> 8;
+        buf[3] = dat;
+        pins.i2cWriteBuffer(AT24_I2C_ADDR, buf)
+    }
+
+    /**
+     * 从指定地址读取字
+     * @param addr eeprom address, eg: 2
+     */
+    //% blockId="AT24_ReadWord" block="读取字从地址 %addr"
+    //% weight=89 blockGap=8
+    export function read_word(addr: number): number {
+        pins.i2cWriteNumber(AT24_I2C_ADDR, addr, NumberFormat.UInt16BE);
+        return pins.i2cReadNumber(AT24_I2C_ADDR, NumberFormat.UInt16BE);
+    }
+
+    /**
+     * 写入双字到指定地址
+     * @param addr eeprom address, eg: 4
+     * @param dat is the data will be write, eg: 7
+     */
+    //% blockId="AT24_WriteDWord" block="eeprom 地址 %addr|写入双字 %dat"
+    //% weight=80 blockGap=8
+    export function write_dword(addr: number, dat: number): void {
+        let buf = pins.createBuffer(6);
+        buf[0] = addr >> 8;
+        buf[1] = addr;
+        buf[2] = dat >> 24;
+        buf[3] = dat >> 16;
+        buf[4] = dat >> 8;
+        buf[5] = dat;
+        pins.i2cWriteBuffer(AT24_I2C_ADDR, buf)
+    }
+
+    /**
+     * 从指定地址读取双字
+     * @param addr eeprom address, eg: 4
+     */
+    //% blockId="AT24_ReadDWord" block="读取双字从地址 %addr"
+    //% weight=79 blockGap=8
+    export function read_dword(addr: number): number {
+        pins.i2cWriteNumber(AT24_I2C_ADDR, addr, NumberFormat.UInt16BE);
+        return pins.i2cReadNumber(AT24_I2C_ADDR, NumberFormat.Int32BE);
+    }
+}
